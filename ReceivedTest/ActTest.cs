35d0790f7d7760f17758ddf5c97d06bcef21c67a
Rainy <rainy4499@gmail.com>
2020-03-17 08:43:31 +0800

    recevied test

---
+++ ReceivedTest/ActTest.cs
@@ -0,0 +1,30 @@
+﻿namespace ReceivedTest
+{
+    public class ActTest
+    {
+        public virtual void Add()
+        {
+            return;
+        }
+    }
+
+    public class FakeActTest : ActTest
+    {
+        private int _callCounts;
+        public override void Add()
+        {
+            _callCounts++;
+            base.Add();
+        }
+
+        public bool Recevied(int receviedTimes)
+        {
+            return _callCounts == receviedTimes;
+        }
+
+        public bool Recevied()
+        {
+            return _callCounts == 1;
+        }
+    }
+}
---
+++ ReceivedTest/Tests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace ReceivedTest
+{
+    [TestFixture]
+    public class Tests
+    {
+        private FakeActTest _actTest;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _actTest = new FakeActTest();
+        }
+
+        [Test]
+        public void recevied_without_parameter()
+        {
+            _actTest.Add();
+            Assert.IsTrue(_actTest.Recevied());
+        }
+        
+        [Test]
+        public void recevied_1_but_method_is_not_call_fail()
+        {
+            Assert.IsFalse(_actTest.Recevied(1));
+        }
+        
+        [Test]
+        public void recevied_3()
+        {
+            _actTest.Add();
+            _actTest.Add();
+            _actTest.Add();
+            Assert.IsTrue(_actTest.Recevied(3));
+        }
+    }
+}
